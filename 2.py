bc57c1c6703fd8270c0157f652b447643cf375e5
S.Nrusimha Vedeep <117562222+snvedeep05@users.noreply.github.com>
2023-06-23 09:40:25 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-for index, row in df.iterrows():
-    # Access row data using row[column_name]
-    # Example: row['column_name']
-    print(row['column_name'])
-for column_name in df:
-    # Access column data using df[column_name]
-    # Example: df[column_name]
-    print(df[column_name])
